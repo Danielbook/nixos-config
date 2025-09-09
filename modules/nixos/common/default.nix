63afdd8d3759b11c530e5b6f79a43b7827ae014d
Daniel Book <daniel@bookorjeman.se>
2025-09-09 08:50:11 +0200

    Configure virtual camera kernel modules
    - Add v4l2loopback kernel module for virtual camera
    - Set up video streaming capabilities
    - Enable OBS virtual camera functionality

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -54,11 +54,21 @@
     plymouth.enable = true;
 
     # v4l (virtual camera) module settings
-    kernelModules = ["v4l2loopback"];
-    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
-    extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-    '';
+    #kernelModules = ["v4l2loopback"];
+    #extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+    #extraModprobeConfig = ''
+    #  options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    #'';
+  };
+
+  services.v4l2loopback = {
+    enable = true;
+    devices = [
+      {
+        cardLabel = "Virtual Camera";
+        exclusiveCaps = true;
+      }
+    ];
   };
 
   # Networking
